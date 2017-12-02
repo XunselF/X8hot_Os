ba2a84dcb794fbf31f2ba0b53399754f372f896a
XunselF <XunselF@hotmail.com>
2017-12-02 16:58:22 +0800

    add mainActivity about item UI

--- app/src/main/java/com/example/xunself/x8hot_os/MainActivity.java
+++ app/src/main/java/com/example/xunself/x8hot_os/MainActivity.java
@@ -3,11 +3,19 @@ package com.example.xunself.x8hot_os;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.PopupMenu;
 import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private int network_status;
+
+    private ImageButton imageButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +23,32 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = getIntent();
         network_status = intent.getIntExtra("network_status",0);
         Log.d("network_status",network_status+"");
+
+        imageButton = (ImageButton) findViewById(R.id.popup_button);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                PopupMenu popupMenu = new PopupMenu(MainActivity.this,imageButton);
+                popupMenu.getMenuInflater().inflate(R.menu.popup_menu,popupMenu.getMenu());
+                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                    @Override
+                    public boolean onMenuItemClick(MenuItem item) {
+                        switch (item.getItemId()){
+                            case R.id.add_item:
+                                Toast.makeText(MainActivity.this,"add",Toast.LENGTH_SHORT).show();
+                                break;
+                            case R.id.about_item:
+                                Toast.makeText(MainActivity.this,"about",Toast.LENGTH_SHORT).show();
+                                break;
+                            default:
+                                break;
+                        }
+                        return true;
+                    }
+                });
+                popupMenu.show();
+                
+            }
+        });
     }
 }
